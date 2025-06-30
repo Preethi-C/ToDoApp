e54f7b674ddff52971f0861cfe5d12bf4c0cd461
Preethi-C <cpreethi1999@gmail.com>
2025-06-30 19:35:13 +0530

    Create ApplicationDbContext class

---
+++ ToDoApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        DbSet<ToDo> ToDos { get; set; }
+    }
+}
