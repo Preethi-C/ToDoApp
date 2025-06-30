262a49867216cc2d0f29eba856e94824a9374e5f
Preethi-C <cpreethi1999@gmail.com>
2025-06-30 19:08:59 +0530

    Add project files.

---
+++ ToDoApp/Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted {  get; set; }
+    }
+}
