2884c6d4e4a62df356569588cdf8b5332541c28d
Preethi-C <cpreethi1999@gmail.com>
2025-07-02 19:27:50 +0530

    create edit view

--- ToDoApp/Controllers/ToDoController.cs
+++ ToDoApp/Controllers/ToDoController.cs
@@ -28,19 +28,32 @@ namespace ToDoApp.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
-        [HttpPost]
-        public IActionResult ToggleComplete(int Id, bool IsDone)
-        {
-        ToDo? todoObj = _db.ToDos.Find(Id);
-            if (todoObj == null)
+
+        public IActionResult Edit(int? Id)
+        {   
+            if(Id==0 || Id==null)
             {
                 return NotFound();
             }
-            todoObj.IsDone = IsDone;
-            _db.SaveChanges();
-
-            return RedirectToAction("Index");
+            ToDo? todoobj = _db.ToDos.FirstOrDefault(x => x.Id==Id);
+            if(todoobj==null)
+            {
+                return NotFound();
+            }
+            return View(todoobj);
         }
+        [HttpPost,ActionName("Edit")]
+        public IActionResult EditPOST(ToDo obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ToDos.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        
 
     }
 }
